d9df12e31d184ae9c4c9a5815488f6399f83db00
Meherin-Islam <meherinislam59@gmail.com>
2024-12-30 21:53:25 +0600

    Popular Menu Created

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { router } from './Routes/Routes';
 import { RouterProvider } from 'react-router-dom';
-// import { HelmetProvider } from 'react-helmet-async';
+ import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   {/* <HelmetProvider> */}
+   <HelmetProvider>
     <div className='max-w-screen-xl mx-auto'>
     <RouterProvider router={router} />
     </div>
-    {/* </HelmetProvider> */}
+    </HelmetProvider>
   </StrictMode>,
 )
