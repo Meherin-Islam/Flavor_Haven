e2be167a8dd98bfbf182470332226b3c12b2b82c
Meherin-Islam <meherinislam59@gmail.com>
2024-12-30 20:41:20 +0600

    Initial Route SetUp Done

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import {
+    createBrowserRouter,
+
+  } from "react-router-dom";
+
+  export  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+    ]
+},
+]);
