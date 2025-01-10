3a8b8cf28e4dbc4bc240a9f5098b844de5054933
Meherin-Islam <meherinislam59@gmail.com>
2025-01-10 21:29:04 +0600

    Route for admin added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from "./PrivateRoute";
 import Secret from "../Pages/Shared/Secret/Secret";
 import Dashboard from "../Layout/Dashboard";
 import Cart from "../Pages/Dashboard/Cart/Cart";
+import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
 
   export  const router = createBrowserRouter([
     {
@@ -52,7 +53,13 @@ import Cart from "../Pages/Dashboard/Cart/Cart";
     {
       path: 'cart',
       element: <Cart></Cart>
-    }
+    },
+   // admin routes
+   {
+    path: 'users',
+    element: <AllUsers></AllUsers>
+  }
+
   ]
 }
 
